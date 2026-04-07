1f65ebf8648d6685898f315c048063f084469fe1
Shubham Paul <paulshubham779@gmail.com>
2026-04-07 14:34:20 +0530

    new update

--- 17_JWTConnectingFEBEPractice/frontend/src/App.jsx
+++ 17_JWTConnectingFEBEPractice/frontend/src/App.jsx
@@ -24,6 +24,26 @@ function App() {
     alert(response.data.msg);
   }
 
+  const handelSignin = async(e)=>{
+    e.preventDefault();
+    const response = await axios.post("http://localhost:3000/signin",{
+      username: signinusername,
+      password:signinpassword
+    })
+    settoken(localStorage.setItem("token",response.data.token));
+    alert("Signin Successfull!!!")
+  }
+
+  const getUserInfo = async(e)=>{
+    e.preventDefault();
+    const response = await axios.get("http://localhost:3000/me",{
+      headers:{
+        token:localStorage.getItem("token")
+      }
+    })
+    setvalue(response.data);
+  }
+
   return (
     <div>
       <h1>Signup</h1>
@@ -33,6 +53,17 @@ function App() {
          <button>Signup</button>
        </form>
       
+      <h1>Signin</h1>
+      <form  onSubmit={handelSignin}  action="">
+        <input onChange={(event)=>setsigninusername(event.target.value)} type="text" placeholder='username' value={signinusername} />
+        <input onChange={(event)=>setsigninpassword(event.target.value)} type="text" placeholder='password' value={signinpassword} />
+        <button>Signin</button>
+      </form>
+      
+      <h1>User Information</h1>
+      <p>{value.username}</p>
+       <p>{value.password}</p>
+      <button onClick={getUserInfo} > Get Info</button>
 
     </div>
   )
