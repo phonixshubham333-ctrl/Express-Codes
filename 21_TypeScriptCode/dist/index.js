0304e5edc628eef190138bfe7a572314b187961a
Shubham Paul <paulshubham779@gmail.com>
2026-04-17 15:53:04 +0530

    second commit

--- 21_TypeScriptCode/dist/index.js
+++ 21_TypeScriptCode/dist/index.js
@@ -1,48 +1,22 @@
 "use strict";
 // let x:number =1;
 Object.defineProperty(exports, "__esModule", { value: true });
-// console.log(x);
-// let shout:string ="Hello";
-// console.log(shout)
-// function sum(a:number , b:number) : number{
-//     return a+b
-// }
-// let ans = sum(1,2);
-// console.log(ans);
-// function delayedCall (anotherFn : ()=> void ){
-//     setTimeout(anotherFn,2000);
-// }
-// function log(){
-//     console.log("hi there!!!")
-// }
-// delayedCall(log);
-// function delaycall(anotherfn : ()=>void ){
-//     setTimeout(anotherfn,5000)
-// }
-// function log1():void{
-//     console.log("Hello My name is Shubham")
-// }
-// delaycall(log1);
-// let user :{
-//     firstname:string,
-//     age:number
-// } = {
-//     firstname:"harkirat",
-//     age:21
-// }
-//Interface ------->
 const user = {
-    firstname: "Shubham",
-    lastname: "paul",
-    age: 25
+    name: "Shubham ",
+    age: 25,
+    address: {
+        city: "Kotulpur",
+        country: "India",
+        pincode: 564751
+    }
 };
-function islegal(user) {
-    if (user.age > 18) {
+function isMyUserProper(user) {
+    if (user.name.trim() == "Shubham" && user.age == 25) {
         return true;
     }
     else {
         return false;
     }
 }
-console.log(islegal(user));
+console.log(isMyUserProper(user));
 //# sourceMappingURL=index.js.map
--- 21_TypeScriptCode/src/index.ts
+++ 21_TypeScriptCode/src/index.ts
@@ -1,78 +1,108 @@
-let x:number =1;
+// let x:number =1;
 
-console.log(x);
+// console.log(x);
 
-let shout:string ="Hello";
+// let shout:string ="Hello";
 
-console.log(shout)
+// console.log(shout)
 
 
-function sum(a:number , b:number) : number{
-    return a+b
+// function sum(a:number , b:number) : number{
+//     return a+b
+// }
+
+// let ans = sum(1,2);
+
+// console.log(ans);
+
+// function delayedCall (anotherFn : ()=> void ){
+//     setTimeout(anotherFn,2000);
+// }
+
+// function log(){
+//     console.log("hi there!!!")
+// }
+
+// delayedCall(log);
+
+
+// function delaycall(anotherfn : ()=>void ){
+//     setTimeout(anotherfn,5000)
+// }
+
+// function log1():void{
+//     console.log("Hello My name is Shubham")
+// }
+
+// delaycall(log1);
+
+// let user :{
+//     firstname:string,
+//     age:number
+// } = {
+//     firstname:"harkirat",
+//     age:21
+// }
+
+// Interface ------->
+// const user ={
+//     firstname:"Shubham",
+//     lastname:"paul",
+//     age:25
+// }
+
+// interface User {
+//     firstname:string;
+//     lastname:string;
+//     age:number;
+// }
+
+// function islegal(user:User){
+//     if(user.age >18){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(islegal(user));
+
+// //types ------>
+// type Alltype = number | string
+
+// function sum(a:Alltype , b:Alltype){
+//     return a+b   //Errror Gives natural 
+// }
+
+interface Address {
+    city:string;
+    country:string,
+    pincode:number
 }
-
-let ans = sum(1,2);
-
-console.log(ans);
-
-function delayedCall (anotherFn : ()=> void ){
-    setTimeout(anotherFn,2000);
-}
-
-function log(){
-    console.log("hi there!!!")
-}
-
-delayedCall(log);
-
-
-function delaycall(anotherfn : ()=>void ){
-    setTimeout(anotherfn,5000)
-}
-
-function log1():void{
-    console.log("Hello My name is Shubham")
-}
-
-delaycall(log1);
-
-let user :{
-    firstname:string,
-    age:number
-} = {
-    firstname:"harkirat",
-    age:21
-}
-
-Interface ------->
-const user ={
-    firstname:"Shubham",
-    lastname:"paul",
-    age:25
-}
-
 interface User {
-    firstname:string;
-    lastname:string;
+    name:string;
     age:number;
+    address:Address
 }
-
-function islegal(user:User){
-    if(user.age >18){
+const user ={
+    name:"Shubham ",
+    age:25,
+    address:{
+        city:"Kotulpur",
+        country:"India",
+        pincode:564751
+    }
+}
+function isMyUserProper (user:User){
+      if(user.name.trim()=="Shubham" && user.age==25){
         return true
-    }
-    else{
+      }
+      else{
         return false
-    }
+      }
 }
 
-console.log(islegal(user));
-
-//types ------>
-type Alltype = number | string
-
-function sum(a:Alltype , b:Alltype){
-    return a+b   //Errror Gives natural 
-}
+console.log(isMyUserProper(user));
 
 
