73f430282e8f6b0d4c6e305727c1b2ceb86b3608
Shubham Paul <paulshubham779@gmail.com>
2026-04-20 12:35:35 +0530

    complex number logic updated

--- 22_ComplexNumberUsingTs/index.js
+++ 22_ComplexNumberUsingTs/index.js
@@ -8,9 +8,21 @@ class ComplexNumber {
         this.imaginary = imaginary;
     }
     display() {
-        console.log("Complex number is " + this.real + "+i" + this.imaginary);
+        console.log("Complex number is " + this.real + "+" + this.imaginary + "i");
+    }
+    add(C) {
+        this.real += C.real;
+        this.imaginary += C.imaginary;
+    }
+    multiply(C) {
+        const r = this.real;
+        const i = this.imaginary;
+        this.real = (r * C.real - C.imaginary * i);
+        this.imaginary = (r * C.imaginary + i * C.real);
     }
 }
 const c1 = new ComplexNumber(5, 10);
+c1.add(new ComplexNumber(2, 1));
+c1.multiply(new ComplexNumber(3, 4));
 c1.display();
 //# sourceMappingURL=index.js.map
--- 22_ComplexNumberUsingTs/index.ts
+++ 22_ComplexNumberUsingTs/index.ts
@@ -8,9 +8,23 @@ class ComplexNumber {
     }
 
     display():void{
-        console.log("Complex number is "+ this.real + "+i"+this.imaginary)
+        console.log("Complex number is "+ this.real+"+"+this.imaginary+ "i")
+    }
+
+    add(C:ComplexNumber):void{
+           this.real+=C.real
+           this.imaginary+=C.imaginary
+    }
+
+    multiply(C:ComplexNumber):void{
+        const r = this.real;
+        const i = this.imaginary;
+        this.real=(r*C.real - C.imaginary*i)
+        this.imaginary=(r*C.imaginary + i*C.real)
     }
 }
 
 const c1:ComplexNumber = new ComplexNumber(5,10);
+c1.add(new ComplexNumber(2,1));
+c1.multiply(new ComplexNumber(3,4));
 c1.display();
