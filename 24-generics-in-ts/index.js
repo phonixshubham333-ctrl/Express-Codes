508b298d7bcaa93c419c010b7d8f6db66fe66c88
Shubham Paul <paulshubham779@gmail.com>
2026-04-20 15:00:49 +0530

    generics

---
+++ 24-generics-in-ts/index.js
@@ -0,0 +1,43 @@
+"use strict";
+//generics----> <T>
+// function linearSearch <T>(array:T[] , x:T):[number ,T |undefined]{
+//     for(let i=0;i<array.length;i++){
+//         if(array[i]==x){
+//             return[i,array[i]];
+//         }
+//     }
+//     return[-1,x]
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// const array:number[] =[2,4,7,8,56,-2,65,745,-1,68];
+// console.log(linearSearch<number>(array,8));
+// const array1:string[]=["jwd","Shubham", "jshdwd","hello","i"];
+// console.log(linearSearch<string>(array1,"Shubham"));
+//Generics in Stack --->
+class Stack {
+    array;
+    constructor() {
+        this.array = [];
+    }
+    push(x) {
+        this.array.push(x);
+    }
+    pop() {
+        this.array.pop();
+    }
+    top() {
+        return this.array[this.array.length - 1];
+    }
+    display() {
+        console.log(this.array);
+    }
+}
+const st = new Stack();
+st.push("Shubham");
+st.push("i");
+st.push("g");
+st.push("Hello");
+st.display();
+st.pop();
+st.display();
+//# sourceMappingURL=index.js.map
